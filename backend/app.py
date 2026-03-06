c26be2040220c75f5dfd9bcf6db60c5b99bdf289
wlfdg <wolfpetergarcia2023@Icloud.com>
2026-03-06 23:46:18 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -688,5 +688,4 @@ def report_excel():
     response.headers["Content-Type"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     return response
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
